cbdd8023e4801054cae80b4f5d11d2c63b85ac06
alishaunair <jihan.alisha14@gmail.com>
2026-08-26 03:48:14 +0700

    Memperbaiki tampilan peminjaman

--- peminjaman/index.php
+++ peminjaman/index.php
@@ -27,10 +27,6 @@ $query = mysqli_query($koneksi, "
 
 <link rel="stylesheet" href="../style.css">
 
-<link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
->
 </head>
 
 <body>
